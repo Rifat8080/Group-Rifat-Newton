fd48aaab875bf024dcf88c284ebea07710746fde
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-01-24 17:20:07 +0600

    Update trials.js

--- trials.js
+++ trials.js
@@ -1,1 +1,18 @@
 "use strict";
+// Extra Long Factorials Project
+function extraLongFactorials(n) {
+  let result = [BigInt(0), BigInt(1)];
+
+  // Arrow function for the conditional factorial
+
+  const factorial = (num) =>
+    typeof result[num] !== "number"
+      ? num - BigInt(1) > 0
+        ? num * factorial(num - BigInt(1))
+        : BigInt(1)
+      : result[num];
+
+  console.log(String(factorial(BigInt(n))));
+}
+
+extraLongFactorials(25);
