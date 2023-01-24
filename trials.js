8591a82e26daf72ab6d33c9ffed3708c661b0210
Newton Ombese <newtonombese1@gmail.com>
2023-01-24 14:32:34 +0300

    worked on the minimum distance code challenge

--- trials.js
+++ trials.js
@@ -16,3 +16,32 @@ function extraLongFactorials(n) {
 }
 
 extraLongFactorials(25);
+
+
+// Minimum Distance Code Challenge
+function minimumDistances(a){
+  // Declaring an empty array
+  let distances = []
+
+  // for loop to help in iteration
+  for(let i = 0; i < a.length; i++){
+    if(a.indexOf(a[i]) !== a.lastIndexOf(a[i])){
+      let minidistance = a.lastIndexOf(a[i]) - a.indexOf(a[i]);
+        distances.push(minidistance)
+    }
+  }
+
+  if(distances.length === 0){
+    return -1;
+  }
+  else{
+    distances.sort(function(a, b){
+      return a - b;
+    });
+
+    return distances[0];
+  }
+
+}
+
+minimumDistances(34)
